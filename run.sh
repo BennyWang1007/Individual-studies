6e89fea07220f15f298e408f6e209008527dfa34
unknown <bennywang1007@gmail.com>
2025-04-23 23:38:58 +0800

    Remove \r in run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python local_data_generate.py &&\
-python local_data_format.py &&\
-python local_data_format.py &&\
-python local_data_format.py &&\
-python local_data_format.py &&\
-python local_data_format.py &&\
-python split_train_eval_data.py &&\
+python local_data_generate.py
+
+for i in {1..5}; do
+    python local_data_format.py
+done
+
+python split_train_eval_data.py
 python curriculum_main.py
